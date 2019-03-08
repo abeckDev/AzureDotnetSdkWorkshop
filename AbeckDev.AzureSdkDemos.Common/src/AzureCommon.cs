11ea6e70a112b03bcb7ca9761f096316f9c29be3
Alexander Beck <github@beck-alexander.de>
2019-03-08 17:53:22 +0100

    Project Layout Added Project structure Added Entry Class Added Config File for Tests

---
+++ AbeckDev.AzureSdkDemos.Common/src/AzureCommon.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using System;
+
+namespace AbeckDev.AzureSdkDemos.Common
+{
+    public static class AzureCommon
+    {
+        public static IAzure GetAzureInterface(string clientId, string clientSecret, string tenantId, string subscriptionId = null)
+        {
+
+            AzureCredentials azureCredentials = new AzureCredentialsFactory()
+                .FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
+
+            var authenticated = Azure.Authenticate(azureCredentials);
+
+            if (subscriptionId == null)
+            {
+                return authenticated.WithDefaultSubscription();
+            }
+
+            return authenticated.WithSubscription(subscriptionId);
+        }
+    }
+}
---
+++ AbeckDev.AzureSdkDemos.Common/test/AzureCommonTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AbeckDev.AzureSdkDemos.Common.Test
+{
+    public class AzureCommonTests
+    {
+        [Fact]
+        public void TestInterfaceAssignment()
+        {
+
+        }
+    }
+}
