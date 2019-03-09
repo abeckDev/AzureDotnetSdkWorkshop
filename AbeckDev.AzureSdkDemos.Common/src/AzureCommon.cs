fb14d8c8e40bf06d76251d40913ec65cfcd96071
Alexander Beck <github@beck-alexander.de>
2019-03-09 11:27:30 +0100

    Updated AzureCommon.cs Added ARM deployment method Added documentqation

--- AbeckDev.AzureSdkDemos.Common/src/AzureCommon.cs
+++ AbeckDev.AzureSdkDemos.Common/src/AzureCommon.cs
@@ -1,26 +1,53 @@
 ﻿using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Deployment.Definition;
 using System;
 
 namespace AbeckDev.AzureSdkDemos.Common
 {
     public static class AzureCommon
     {
+        /// <summary>
+        /// Gets an <see cref="IAzure"/> by authenticating against Azure AD with a Service Principal
+        /// </summary>
+        /// <param name="clientId">The clientId from the AppRegistration in AzureAD</param>
+        /// <param name="clientSecret">The clientSecret from the AppRegistration in AzureAD</param>
+        /// <param name="tenantId">The Azure TenantId aka DirectoryId</param>
+        /// <param name="subscriptionId">(optional) The Subscription to use. Default one is used if empty</param>
+        /// <returns>An <see cref="IAzure"/> Interface for communication with the Azure Cloud</returns>
         public static IAzure GetAzureInterface(string clientId, string clientSecret, string tenantId, string subscriptionId = null)
         {
-
             AzureCredentials azureCredentials = new AzureCredentialsFactory()
                 .FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
-
             var authenticated = Azure.Authenticate(azureCredentials);
-
             if (subscriptionId == null)
             {
                 return authenticated.WithDefaultSubscription();
             }
-
             return authenticated.WithSubscription(subscriptionId);
+        }
+
+        /// <summary>
+        /// Deploys an Azure Resource Manager (ARM) Template based on an existing Azure connection.
+        /// </summary>
+        /// <param name="azure">The <see cref="IAzure"/> Interface from the existing connection</param>
+        /// <param name="ResourceGroupName">The Name of the ResourceGroup where to deploy the Resources</param>
+        /// <param name="templateJson">The ARM Template File in Json</param>
+        /// <param name="parametersJson">The Parameters File in Json</param>
+        /// <returns>An <see cref="IDeployment"/> Object with information of the deployment operation in Azure</returns>
+        public static IDeployment DeployArmTemplate(IAzure azure, string ResourceGroupName, string templateJson, string parametersJson)
+        {
+            string deploymentName = ResourceGroupName + "-" + Guid.NewGuid().ToString();
+            var deployment = azure.Deployments.Define(deploymentName)
+                .WithExistingResourceGroup(ResourceGroupName)
+                .WithTemplate(templateJson)
+                .WithParameters(parametersJson)
+                .WithMode(Microsoft.Azure.Management.ResourceManager.Fluent.Models.DeploymentMode.Incremental)
+                .BeginCreate();
+            var result = azure.Deployments
+                .GetByName(deploymentName);
+            return result;
         }
     }
 }
