0aa959daf7d83a48410809393e3aada460116b21
Alexander Beck <github@beck-alexander.de>
2019-03-09 12:46:05 +0100

    AzureCommon updates

--- AbeckDev.AzureSdkDemos.Common/src/AzureCommon.cs
+++ AbeckDev.AzureSdkDemos.Common/src/AzureCommon.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Azure.Management.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
 using Microsoft.Azure.Management.ResourceManager.Fluent.Deployment.Definition;
 using System;
+using System.Collections.Generic;
 
 namespace AbeckDev.AzureSdkDemos.Common
 {
@@ -32,14 +34,18 @@ namespace AbeckDev.AzureSdkDemos.Common
         /// Deploys an Azure Resource Manager (ARM) Template based on an existing Azure connection.
         /// </summary>
         /// <param name="azure">The <see cref="IAzure"/> Interface from the existing connection</param>
-        /// <param name="ResourceGroupName">The Name of the ResourceGroup where to deploy the Resources</param>
+        /// <param name="ResourceGroupName">The Name of the ResourceGroup where to deploy the Resources. Will be created if not existing</param>
         /// <param name="templateJson">The ARM Template File in Json</param>
         /// <param name="parametersJson">The Parameters File in Json</param>
         /// <returns>An <see cref="IDeployment"/> Object with information of the deployment operation in Azure</returns>
         public static IDeployment DeployArmTemplate(IAzure azure, string ResourceGroupName, string templateJson, string parametersJson)
         {
+            if (!azure.ResourceGroups.Contain(ResourceGroupName))
+            {
+                CreateResourceGroup(azure, ResourceGroupName);
+            }
             string deploymentName = ResourceGroupName + "-" + Guid.NewGuid().ToString();
-            var deployment = azure.Deployments.Define(deploymentName)
+            azure.Deployments.Define(deploymentName)
                 .WithExistingResourceGroup(ResourceGroupName)
                 .WithTemplate(templateJson)
                 .WithParameters(parametersJson)
@@ -48,6 +54,24 @@ namespace AbeckDev.AzureSdkDemos.Common
             var result = azure.Deployments
                 .GetByName(deploymentName);
             return result;
+        }
+
+        public static IResourceGroup CreateResourceGroup(IAzure azure, string ResourceGroupName)
+        {
+            if (azure.ResourceGroups.Contain(ResourceGroupName))
+            {
+                //ResourceGroup exists abort
+                throw new Exception("The ResourceGroup exist already");
+            }
+
+            Dictionary<string, string> tags = new Dictionary<string, string>();
+            tags.Add("Sample Tag Key", "Sample Tag Value");
+            tags.Add("CreationDate", DateTime.Now.Date.ToLongDateString());
+
+            return azure.ResourceGroups.Define(ResourceGroupName)
+                .WithRegion(Region.EuropeWest)
+                .WithTags(tags)
+                .Create();
         }
     }
 }
